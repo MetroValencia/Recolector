7b35d7e0ab96aa09dbe42049ba8c87817ef0dd30
Emanuel Ciotoi <emanuel12m@gmail.com>
2022-06-11 16:22:53 +0200

    Nuevo formato fecha

--- Javascript/xlsxJson.js
+++ Javascript/xlsxJson.js
@@ -61,21 +61,54 @@ function wBtoJson(workBook) {
  */
 function structuredData(json, titles) {
 	const arr = [];
-	const arr2 = [['Mes', 'Año', 'Linea', 'Pasajeros']];
+	const arr2 = [['Mes/Año', 'Líneas', 'Pasajeros']];
 	const values = json.map(filterObject);
+	const meses = getMonths();
 
 	arr.push(...values.sort((a, b) => a[1] - b[1]));
 
 	for (const i in arr) {
+		const mesAnyo = getDate(arr[i][1], arr[i][0], meses);
+
 		for (const j in arr[i]) {
 			if (j == 0 || j == 1) continue;
 
-			arr2.push([arr[i][0], arr[i][1], titles[j], arr[i][j]]);
+			arr2.push([mesAnyo, titles[j], arr[i][j]]);
 		}
 	}
 
 	return arr2;
 }
+
+/**
+ * Crea un objeto congelado para emular un enum de los meses del año
+ * @returns Meses en español(España) con valor numérico de `Date`
+ */
+function getMonths() {
+	const nMeses = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((mon) =>
+		new Date(2022, mon).toLocaleString('es-ES', { month: 'long' }),
+	);
+	const meses = {};
+
+	for (const key in nMeses) {
+		meses[nMeses[key]] = parseInt(key);
+	}
+
+	return Object.freeze(meses);
+}
+
+/**
+ * Crea un string de fecha (mes/año)
+ * @param {Number} year Año del dato actual
+ * @param {String} month Mes del dato actual
+ * @param {Object} months Listado de meses con valor numérico
+ * @returns String de mes y año con valor numérico
+ */
+const getDate = (year, month, months) =>
+	new Date(year, months[month.toLowerCase()]).toLocaleDateString('es-ES', {
+		year: 'numeric',
+		month: 'numeric',
+	});
 
 /**
  * Transforma un JSON y extrae a un array los valores
